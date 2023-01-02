d55235f5c86809d55043368fe3189bf18db27d12
Mike Ralph <michael_ralph@hotmail.com>
2023-01-02 09:04:32 +0000

    Add champion ability data - styling required

--- src/pages/champion/ChampionPage.js
+++ src/pages/champion/ChampionPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import styles from "../../styles/ChampionPage.module.css";
@@ -11,19 +11,89 @@ const ChampionPage = () => {
     alias: "",
     champ_image: "",
     lore: "",
+    passive_ability: "",
+    passive_ability_description: "",
+    passive_ability_image: "",
+    ability_1: "",
+    ability_1_description: "",
+    ability_1_image: "",
+    ability_2: "",
+    ability_2_description: "",
+    ability_2_image: "",
+    ability_3: "",
+    ability_3_description: "",
+    ability_3_image: "",
+    ultimate_ability: "",
+    ultimate_ability_description: "",
+    ultimate_ability_image: "",
   });
-  const { name, alias, champ_image, lore } = champData;
+  const {
+    name,
+    alias,
+    champ_image,
+    lore,
+    passive_ability,
+    passive_ability_description,
+    passive_ability_image,
+    ability_1,
+    ability_1_description,
+    ability_1_image,
+    ability_2,
+    ability_2_description,
+    ability_2_image,
+    ability_3,
+    ability_3_description,
+    ability_3_image,
+    ultimate_ability,
+    ultimate_ability_description,
+    ultimate_ability_image,
+  } = champData;
 
   useEffect(() => {
     const fetchChampion = async () => {
       try {
         const { data } = await axiosReq.get(`/champions/${id}`);
-        const { name, alias, champ_image, lore } = data;
+        const {
+          name,
+          alias,
+          champ_image,
+          lore,
+          passive_ability,
+          passive_ability_description,
+          passive_ability_image,
+          ability_1,
+          ability_1_description,
+          ability_1_image,
+          ability_2,
+          ability_2_description,
+          ability_2_image,
+          ability_3,
+          ability_3_description,
+          ability_3_image,
+          ultimate_ability,
+          ultimate_ability_description,
+          ultimate_ability_image,
+        } = data;
         setChampData(() => ({
           name: name,
           alias: alias,
           champ_image: champ_image,
           lore: lore,
+          passive_ability: passive_ability,
+          passive_ability_description: passive_ability_description,
+          passive_ability_image: passive_ability_image,
+          ability_1: ability_1,
+          ability_1_description: ability_1_description,
+          ability_1_image: ability_1_image,
+          ability_2: ability_2,
+          ability_2_description: ability_2_description,
+          ability_2_image: ability_2_image,
+          ability_3: ability_3,
+          ability_3_description: ability_3_description,
+          ability_3_image: ability_3_image,
+          ultimate_ability: ultimate_ability,
+          ultimate_ability_description: ultimate_ability_description,
+          ultimate_ability_image: ultimate_ability_image,
         }));
       } catch (err) {
         console.log(err);
@@ -49,6 +119,42 @@ const ChampionPage = () => {
         <Row className={styles.Alias}>{alias}</Row>
         <hr></hr>
         <Row>{lore}</Row>
+        <hr></hr>
+        <Row>
+          <img
+            src={passive_ability_image}
+            className={`${styles.AbilityImage}`}
+            alt={`${passive_ability}`}
+          ></img>
+        </Row>
+        <Row>
+          <img
+            src={ability_1_image}
+            className={`${styles.AbilityImage}`}
+            alt={`${ability_1}`}
+          ></img>
+        </Row>
+        <Row>
+          <img
+            src={ability_2_image}
+            className={`${styles.AbilityImage}`}
+            alt={`${ability_2}`}
+          ></img>
+        </Row>
+        <Row>
+          <img
+            src={ability_3_image}
+            className={`${styles.AbilityImage}`}
+            alt={`${ability_3}`}
+          ></img>
+        </Row>
+        <Row>
+          <img
+            src={ultimate_ability_image}
+            className={`${styles.AbilityImage}`}
+            alt={`${ultimate_ability}`}
+          ></img>
+        </Row>
       </Container>
     </div>
   );
