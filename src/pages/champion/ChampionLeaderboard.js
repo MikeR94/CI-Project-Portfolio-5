fcc49c8105ffc97bf8333c7330f731b361d4fac2
Mike Ralph <michael_ralph@hotmail.com>
2023-01-06 16:54:49 +0000

    Retrieve champion data and start work on the champion card

--- src/pages/champion/ChampionLeaderboard.js
+++ src/pages/champion/ChampionLeaderboard.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "../../api/axiosDefaults";
+import Avatar from "../../components/Avatar";
+import Aatrox from "../../assets/Aatrox.jpg";
+import { Card, Container } from "react-bootstrap";
 
 const ChampionLeaderboard = () => {
-  return <div className="mt-5">This is the leaderboard page</div>;
+  const [champions, setChampions] = useState({ results: [] });
+
+  useEffect(() => {
+    const fetchChampions = async () => {
+      try {
+        const { data } = await axiosReq.get(`/champions/`);
+        setChampions(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchChampions();
+  }, []);
+
+  console.log(champions);
+  return (
+    <Container className="mt-5 d-flex flex-wrap">
+      <Card className="d-block">
+        <Avatar src={Aatrox}></Avatar>
+        <span>Aatrox</span>
+        <span>The Darkin Blade</span>
+        <span>30</span>
+      </Card>
+      <Card className="d-block">
+        <Avatar src={Aatrox}></Avatar>
+        <span>Aatrox</span>
+        <span>The Darkin Blade</span>
+        <span>30</span>
+      </Card>
+    </Container>
+  );
 };
 
 export default ChampionLeaderboard;
