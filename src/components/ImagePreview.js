d911015b1c5cf3ab5153c5d7d2e69784feed04b1
Mike Ralph <michael_ralph@hotmail.com>
2023-01-13 20:12:20 +0000

    Add a default image if there is no image - proper image to be uploaded

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -1,7 +1,12 @@
 import React from "react";
 import styles from "../styles/ImagePreview.module.css";
+import logo from "../assets/logo.png";
 
 const ImagePreview = ({ src, height = 45, width = 45 }) => {
+  if (src === "") {
+    src = logo;
+  }
+
   return (
     <span>
       <img
