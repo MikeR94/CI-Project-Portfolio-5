049cd73e394d752cf85eba6b621d132c97d6cb3f
Mike Ralph <michael_ralph@hotmail.com>
2023-01-13 20:19:21 +0000

    Add a temporary file icon for when there is no image

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "../styles/ImagePreview.module.css";
-import logo from "../assets/logo.png";
+import fileIcon from "../assets/file.png";
 
 const ImagePreview = ({ src, height = 45, width = 45 }) => {
   if (src === "") {
-    src = logo;
+    src = fileIcon;
   }
 
   return (
