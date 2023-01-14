1dce2a88bf767fb27d7fc4e2a5524c49727b99ef
Mike Ralph <michael_ralph@hotmail.com>
2023-01-14 21:22:14 +0000

    Add loading spinner to ChampionPage

--- src/pages/champion/ChampionPage.js
+++ src/pages/champion/ChampionPage.js
@@ -29,6 +29,7 @@ import { fetchMoreData } from "../../utils/utils";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import btnStyles from "../../styles/Button.module.css";
 import { Link } from "react-router-dom";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
 const ChampionPage = () => {
   const { id } = useParams();
@@ -37,6 +38,7 @@ const ChampionPage = () => {
   const is_owner = currentUser?.username;
   const is_staff = currentUser?.is_staff;
   const history = useHistory();
+  const [hasLoaded, setHasLoaded] = useState(false);
   const profile_avatar = currentUser?.profile_avatar;
   const [champData, setChampData] = useState({ results: [] });
   const {
@@ -126,6 +128,7 @@ const ChampionPage = () => {
           upvotes_count: upvotes_count,
           upvotes_id: upvotes_id,
         }));
+        setHasLoaded(true);
       } catch (error) {
         console.log(error);
         history.push("/");
@@ -254,205 +257,211 @@ const ChampionPage = () => {
 
   return (
     <div>
-      <Container className="md">
-        <Row>
-          <img
-            src={champ_image}
-            className={`${styles.Image}`}
-            alt={`${name}`}
-          ></img>
-        </Row>
-        <Row className={styles.Name}>{name}</Row>
-        <Row className={styles.Alias}>{alias}</Row>
-        {is_staff && (
-          <div>
-            <Link to={`/champion/${id}/edit`}>
-              <Button className={`${btnStyles.Button}`}>Edit</Button>
-            </Link>
-            <Button onClick={handleDelete} className={`${btnStyles.Button}`}>
-              Delete
-            </Button>
-          </div>
-        )}
-        <hr className="mt-5 mb-5"></hr>
-        <Row>{lore}</Row>
-        <hr className="mt-5 mb-5"></hr>
-        <Row className="justify-content-center">
-          <OverlayTrigger
-            placement="bottom"
-            overlay={<Tooltip>{passive_ability_description}</Tooltip>}
-          >
+      {hasLoaded ? (
+        <Container className="md">
+          <Row>
             <img
-              src={passive_ability_image}
-              className={`${styles.AbilityImage}`}
-              alt={`${passive_ability}`}
+              src={champ_image}
+              className={`${styles.Image}`}
+              alt={`${name}`}
             ></img>
-          </OverlayTrigger>
-          <div className={styles.AbilityType}>Passive</div>
-          <div className={styles.AbilityName}>{passive_ability}</div>
-        </Row>
-        <Row className="justify-content-center">
-          <OverlayTrigger
-            placement="bottom"
-            overlay={<Tooltip>{ability_1_description}</Tooltip>}
-          >
-            <img
-              src={ability_1_image}
-              className={`${styles.AbilityImage}`}
-              alt={`${ability_1}`}
-            ></img>
-          </OverlayTrigger>
-          <div className={styles.AbilityType}>Ability 1</div>
-          <div className={styles.AbilityName}>{ability_1}</div>
-        </Row>
-        <Row className="justify-content-center">
-          <OverlayTrigger
-            placement="bottom"
-            overlay={<Tooltip>{ability_2_description}</Tooltip>}
-          >
-            <img
-              src={ability_2_image}
-              className={`${styles.AbilityImage}`}
-              alt={`${ability_1}`}
-            ></img>
-          </OverlayTrigger>
-          <div className={styles.AbilityType}>Ability 2</div>
-          <div className={styles.AbilityName}>{ability_2}</div>
-        </Row>
-        <Row className="justify-content-center">
-          <OverlayTrigger
-            placement="bottom"
-            overlay={<Tooltip>{ability_3_description}</Tooltip>}
-          >
-            <img
-              src={ability_3_image}
-              className={`${styles.AbilityImage}`}
-              alt={`${ability_3}`}
-            ></img>
-          </OverlayTrigger>
-          <div className={styles.AbilityType}>Ability 3</div>
-          <div className={styles.AbilityName}>{ability_3}</div>
-        </Row>
-        <Row className="justify-content-center">
-          <OverlayTrigger
-            placement="bottom"
-            overlay={<Tooltip>{ultimate_ability_description}</Tooltip>}
-          >
-            <img
-              src={ultimate_ability_image}
-              className={`${styles.AbilityImage}`}
-              alt={`${ultimate_ability}`}
-            ></img>
-          </OverlayTrigger>
-          <div className={styles.AbilityType}>Ultimate</div>
-          <div className={styles.AbilityName}>{ultimate_ability}</div>
-        </Row>
-        <hr className="mt-5 mb-5"></hr>
-        <Row className="text-center">
-          <Col>
-            <div>Class</div>
-            <img alt="class icon" src={classIcon}></img>
-            <div className="text-capitalize">{champ_class}</div>
-          </Col>
-          <Col>
-            <div>Range</div>
-            <img
-              alt="range icon"
-              src={rangeImage}
-              className={isMelee ? styles.MeleeIcon : styles.RangeIcon}
-            ></img>
-            <div className="text-capitalize">{range}</div>
-          </Col>
-          <Col>
-            <div>Difficulty</div>
-            <img
-              alt="difficulty icon"
-              src={difficultyImage}
-              className={styles.DifficultyIcon}
-            ></img>
-            <div className="text-capitalize">{difficulty}</div>
-          </Col>
-        </Row>
-        <hr className="mt-5 mb-5"></hr>
-        <Row className={styles.UpVoteTitle}>Up Vote</Row>
-        <Row className="align-items-center">
-          <Col lg={8}>
-            {is_owner && upvotes_id && (
-              <div className={styles.UpVoteText}>
-                Great! You have successfully upvoted for {name}!
-              </div>
-            )}
-            {!upvotes_id ? (
-              <div className={styles.UpVoteText}>
-                Do you like this champion? Make sure you up-vote it by clicking
-                the arrow if you do!
-              </div>
-            ) : !is_owner ? (
-              <div className={styles.UpVoteText}>
-                Do you like this champion? Make sure you up-vote it by clicking
-                the arrow if you do!
-              </div>
-            ) : null}
-          </Col>
-          <Col lg={4}>
-            <div className={styles.UpVoteIcon}>
-              {is_owner && upvotes_id ? (
-                <OverlayTrigger
-                  placement="top"
-                  overlay={<Tooltip>Click to down vote this champion</Tooltip>}
-                >
-                  <span onClick={handleDownVote}>
-                    <i className="fa fa-angle-down"></i>
-                  </span>
-                </OverlayTrigger>
-              ) : (
-                <OverlayTrigger
-                  placement="top"
-                  overlay={<Tooltip>Click to up vote this champion</Tooltip>}
-                >
-                  <span onClick={handleUpVote}>
-                    <i className="fa fa-angle-up"></i>
-                  </span>
-                </OverlayTrigger>
-              )}
+          </Row>
+          <Row className={styles.Name}>{name}</Row>
+          <Row className={styles.Alias}>{alias}</Row>
+          {is_staff && (
+            <div>
+              <Link to={`/champion/${id}/edit`}>
+                <Button className={`${btnStyles.Button}`}>Edit</Button>
+              </Link>
+              <Button onClick={handleDelete} className={`${btnStyles.Button}`}>
+                Delete
+              </Button>
             </div>
-            <div className="text-center">{upvotes_count}</div>
-          </Col>
+          )}
           <hr className="mt-5 mb-5"></hr>
-        </Row>
-        <Row className={styles.CommentTitle}>Have Your Say</Row>
-        {currentUser ? (
-          <CommentCreateForm
-            profile_id={currentUser.profile_id}
-            profile_avatar={profile_avatar}
-            champion={id}
-            setCommentData={setCommentData}
-          />
-        ) : commentData.results.length ? (
-          "Comments"
-        ) : null}
-        {commentData.results.length ? (
-          <InfiniteScroll
-            children={commentData.results.map((comment) => (
-              <Comment
-                key={comment.id}
-                setCommentData={setCommentData}
-                is_staff={is_staff}
-                {...comment}
-              />
-            ))}
-            dataLength={commentData.results.length}
-            // TODO - Add a spinner
-            loader="Loading..."
-            hasMore={!!commentData.next}
-            next={() => fetchMoreData(commentData, setCommentData)}
-          />
-        ) : currentUser ? (
-          <span>No comments yet, be the first to comment!</span>
-        ) : (
-          <span>No comments, sign up and be the first to comment!</span>
-        )}
-      </Container>
+          <Row>{lore}</Row>
+          <hr className="mt-5 mb-5"></hr>
+          <Row className="justify-content-center">
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>{passive_ability_description}</Tooltip>}
+            >
+              <img
+                src={passive_ability_image}
+                className={`${styles.AbilityImage}`}
+                alt={`${passive_ability}`}
+              ></img>
+            </OverlayTrigger>
+            <div className={styles.AbilityType}>Passive</div>
+            <div className={styles.AbilityName}>{passive_ability}</div>
+          </Row>
+          <Row className="justify-content-center">
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>{ability_1_description}</Tooltip>}
+            >
+              <img
+                src={ability_1_image}
+                className={`${styles.AbilityImage}`}
+                alt={`${ability_1}`}
+              ></img>
+            </OverlayTrigger>
+            <div className={styles.AbilityType}>Ability 1</div>
+            <div className={styles.AbilityName}>{ability_1}</div>
+          </Row>
+          <Row className="justify-content-center">
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>{ability_2_description}</Tooltip>}
+            >
+              <img
+                src={ability_2_image}
+                className={`${styles.AbilityImage}`}
+                alt={`${ability_1}`}
+              ></img>
+            </OverlayTrigger>
+            <div className={styles.AbilityType}>Ability 2</div>
+            <div className={styles.AbilityName}>{ability_2}</div>
+          </Row>
+          <Row className="justify-content-center">
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>{ability_3_description}</Tooltip>}
+            >
+              <img
+                src={ability_3_image}
+                className={`${styles.AbilityImage}`}
+                alt={`${ability_3}`}
+              ></img>
+            </OverlayTrigger>
+            <div className={styles.AbilityType}>Ability 3</div>
+            <div className={styles.AbilityName}>{ability_3}</div>
+          </Row>
+          <Row className="justify-content-center">
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>{ultimate_ability_description}</Tooltip>}
+            >
+              <img
+                src={ultimate_ability_image}
+                className={`${styles.AbilityImage}`}
+                alt={`${ultimate_ability}`}
+              ></img>
+            </OverlayTrigger>
+            <div className={styles.AbilityType}>Ultimate</div>
+            <div className={styles.AbilityName}>{ultimate_ability}</div>
+          </Row>
+          <hr className="mt-5 mb-5"></hr>
+          <Row className="text-center">
+            <Col>
+              <div>Class</div>
+              <img alt="class icon" src={classIcon}></img>
+              <div className="text-capitalize">{champ_class}</div>
+            </Col>
+            <Col>
+              <div>Range</div>
+              <img
+                alt="range icon"
+                src={rangeImage}
+                className={isMelee ? styles.MeleeIcon : styles.RangeIcon}
+              ></img>
+              <div className="text-capitalize">{range}</div>
+            </Col>
+            <Col>
+              <div>Difficulty</div>
+              <img
+                alt="difficulty icon"
+                src={difficultyImage}
+                className={styles.DifficultyIcon}
+              ></img>
+              <div className="text-capitalize">{difficulty}</div>
+            </Col>
+          </Row>
+          <hr className="mt-5 mb-5"></hr>
+          <Row className={styles.UpVoteTitle}>Up Vote</Row>
+          <Row className="align-items-center">
+            <Col lg={8}>
+              {is_owner && upvotes_id && (
+                <div className={styles.UpVoteText}>
+                  Great! You have successfully upvoted for {name}!
+                </div>
+              )}
+              {!upvotes_id ? (
+                <div className={styles.UpVoteText}>
+                  Do you like this champion? Make sure you up-vote it by
+                  clicking the arrow if you do!
+                </div>
+              ) : !is_owner ? (
+                <div className={styles.UpVoteText}>
+                  Do you like this champion? Make sure you up-vote it by
+                  clicking the arrow if you do!
+                </div>
+              ) : null}
+            </Col>
+            <Col lg={4}>
+              <div className={styles.UpVoteIcon}>
+                {is_owner && upvotes_id ? (
+                  <OverlayTrigger
+                    placement="top"
+                    overlay={
+                      <Tooltip>Click to down vote this champion</Tooltip>
+                    }
+                  >
+                    <span onClick={handleDownVote}>
+                      <i className="fa fa-angle-down"></i>
+                    </span>
+                  </OverlayTrigger>
+                ) : (
+                  <OverlayTrigger
+                    placement="top"
+                    overlay={<Tooltip>Click to up vote this champion</Tooltip>}
+                  >
+                    <span onClick={handleUpVote}>
+                      <i className="fa fa-angle-up"></i>
+                    </span>
+                  </OverlayTrigger>
+                )}
+              </div>
+              <div className="text-center">{upvotes_count}</div>
+            </Col>
+            <hr className="mt-5 mb-5"></hr>
+          </Row>
+          <Row className={styles.CommentTitle}>Have Your Say</Row>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profile_avatar={profile_avatar}
+              champion={id}
+              setCommentData={setCommentData}
+            />
+          ) : commentData.results.length ? (
+            "Comments"
+          ) : null}
+          {commentData.results.length ? (
+            <InfiniteScroll
+              children={commentData.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  setCommentData={setCommentData}
+                  is_staff={is_staff}
+                  {...comment}
+                />
+              ))}
+              dataLength={commentData.results.length}
+              // TODO - Add a spinner
+              loader="Loading..."
+              hasMore={!!commentData.next}
+              next={() => fetchMoreData(commentData, setCommentData)}
+            />
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments, sign up and be the first to comment!</span>
+          )}
+        </Container>
+      ) : (
+        <LoadingSpinner></LoadingSpinner>
+      )}
     </div>
   );
 };
