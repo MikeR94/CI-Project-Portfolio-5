9ae1af3d11be579a1d064f49dc35fd45eaee4194
Mike Ralph <michael_ralph@hotmail.com>
2023-01-14 08:55:18 +0000

    Retrieving profile data form API

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const ProfilePage = () => {
   const { id } = useParams();
+
+  const [profileData, setProfileData] = useState({});
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        const { data } = await axiosReq.get(`/profiles/${id}`);
+        setProfileData(data);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchProfileData();
+  }, [id, setProfileData]);
+
   return (
     <div>
       <Container>
         <Row className="mt-5">
           <div className="mt-5">
             <span>Current Profile Id = {id}</span>
+            <span>Current Profile Name = {profileData.username}</span>
           </div>
         </Row>
       </Container>
