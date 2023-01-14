94640753b122843797d3e25de0ed38b21543bc01
Mike Ralph <michael_ralph@hotmail.com>
2023-01-14 08:41:52 +0000

    Render the profile ID to the profile page

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const ProfilePage = () => {
+  const { id } = useParams();
   return (
     <div>
       <Container>
         <Row className="mt-5">
           <div className="mt-5">
-            <span>Hello World</span>
+            <span>Current Profile Id = {id}</span>
           </div>
         </Row>
       </Container>
