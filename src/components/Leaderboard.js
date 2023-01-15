f3359355edf2d1e4f182a8bd2fa89ed65e30e00e
Mike Ralph <michael_ralph@hotmail.com>
2023-01-15 12:52:13 +0000

    Add a header to the leaderboard page

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Avatar from "../components/Avatar";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import styles from "../styles/ChampionLeaderboard.module.css";
 import SupportIcon from "../assets/role_icons/support.png";
 import AdcIcon from "../assets/role_icons/adc.png";
@@ -20,90 +20,96 @@ import SpecialistIcon from "../assets/class_icons/specialist.webp";
 
 const Leaderboard = ({ champions }) => {
   return (
-    <Container className={styles.Container}>
-      <table className={styles.LeaderboardResults} width="100%">
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Avatar</th>
-            <th>Name</th>
-            <th>Role</th>
-            <th>Class</th>
-            <th>Difficulty</th>
-            <th>Upvotes</th>
-          </tr>
-        </thead>
-        <tbody>
-          <>
-            {champions.length ? (
-              champions.map((champions) => (
-                <tr key={champions.id}>
-                  <td>{champions.rank}</td>
-                  <td>
-                    <Avatar src={champions.champ_image}></Avatar>
-                  </td>
-                  <td>{champions.name}</td>
-                  <td>
-                    {champions.role === "mid" ? (
-                      <Avatar src={MiddleIcon}></Avatar>
-                    ) : champions.role === "top" ? (
-                      <Avatar src={TopIcon}></Avatar>
-                    ) : champions.role === "jungle" ? (
-                      <Avatar src={JungleIcon}></Avatar>
-                    ) : champions.role === "adc" ? (
-                      <Avatar src={AdcIcon}></Avatar>
-                    ) : champions.role === "support" ? (
-                      <Avatar src={SupportIcon}></Avatar>
-                    ) : null}
-                  </td>
-                  <td>
-                    {champions.champ_class === "fighter" ? (
-                      <Avatar src={FighterIcon}></Avatar>
-                    ) : champions.champ_class === "tank" ? (
-                      <Avatar src={TankIcon}></Avatar>
-                    ) : champions.champ_class === "controller" ? (
-                      <Avatar src={ControllerIcon}></Avatar>
-                    ) : champions.champ_class === "mage" ? (
-                      <Avatar src={MageIcon}></Avatar>
-                    ) : champions.champ_class === "marksman" ? (
-                      <Avatar src={MarksmanIcon}></Avatar>
-                    ) : champions.champ_class === "slayer" ? (
-                      <Avatar src={SlayerIcon}></Avatar>
-                    ) : champions.champ_class === "specialist" ? (
-                      <Avatar src={SpecialistIcon}></Avatar>
-                    ) : null}
-                  </td>
-                  <td>
-                    {champions.difficulty === "low" ? (
-                      <img
-                        className={styles.DifficultyIcon}
-                        alt="difficulty icon"
-                        src={LowDifficultyIcon}
-                      ></img>
-                    ) : champions.difficulty === "moderate" ? (
-                      <img
-                        className={styles.DifficultyIcon}
-                        alt="difficulty icon"
-                        src={ModerateDifficultyIcon}
-                      ></img>
-                    ) : champions.difficulty === "high" ? (
-                      <img
-                        className={styles.DifficultyIcon}
-                        alt="difficulty icon"
-                        src={HighDifficultyIcon}
-                      ></img>
-                    ) : null}
-                  </td>
-                  <td>{champions.upvotes_count}</td>
-                </tr>
-              ))
-            ) : (
-              // TODO - Add a spinner
-              <h1>Loading....</h1>
-            )}
-          </>
-        </tbody>
-      </table>
+    <Container>
+      <Row className="mt-5">
+        <h1 className={styles.Header}>Leaderboard</h1>
+        <hr></hr>
+      </Row>
+      <Container className={styles.Container}>
+        <table className={styles.LeaderboardResults} width="100%">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Role</th>
+              <th>Class</th>
+              <th>Difficulty</th>
+              <th>Upvotes</th>
+            </tr>
+          </thead>
+          <tbody>
+            <>
+              {champions.length ? (
+                champions.map((champions) => (
+                  <tr key={champions.id}>
+                    <td>{champions.rank}</td>
+                    <td>
+                      <Avatar src={champions.champ_image}></Avatar>
+                    </td>
+                    <td>{champions.name}</td>
+                    <td>
+                      {champions.role === "mid" ? (
+                        <Avatar src={MiddleIcon}></Avatar>
+                      ) : champions.role === "top" ? (
+                        <Avatar src={TopIcon}></Avatar>
+                      ) : champions.role === "jungle" ? (
+                        <Avatar src={JungleIcon}></Avatar>
+                      ) : champions.role === "adc" ? (
+                        <Avatar src={AdcIcon}></Avatar>
+                      ) : champions.role === "support" ? (
+                        <Avatar src={SupportIcon}></Avatar>
+                      ) : null}
+                    </td>
+                    <td>
+                      {champions.champ_class === "fighter" ? (
+                        <Avatar src={FighterIcon}></Avatar>
+                      ) : champions.champ_class === "tank" ? (
+                        <Avatar src={TankIcon}></Avatar>
+                      ) : champions.champ_class === "controller" ? (
+                        <Avatar src={ControllerIcon}></Avatar>
+                      ) : champions.champ_class === "mage" ? (
+                        <Avatar src={MageIcon}></Avatar>
+                      ) : champions.champ_class === "marksman" ? (
+                        <Avatar src={MarksmanIcon}></Avatar>
+                      ) : champions.champ_class === "slayer" ? (
+                        <Avatar src={SlayerIcon}></Avatar>
+                      ) : champions.champ_class === "specialist" ? (
+                        <Avatar src={SpecialistIcon}></Avatar>
+                      ) : null}
+                    </td>
+                    <td>
+                      {champions.difficulty === "low" ? (
+                        <img
+                          className={styles.DifficultyIcon}
+                          alt="difficulty icon"
+                          src={LowDifficultyIcon}
+                        ></img>
+                      ) : champions.difficulty === "moderate" ? (
+                        <img
+                          className={styles.DifficultyIcon}
+                          alt="difficulty icon"
+                          src={ModerateDifficultyIcon}
+                        ></img>
+                      ) : champions.difficulty === "high" ? (
+                        <img
+                          className={styles.DifficultyIcon}
+                          alt="difficulty icon"
+                          src={HighDifficultyIcon}
+                        ></img>
+                      ) : null}
+                    </td>
+                    <td>{champions.upvotes_count}</td>
+                  </tr>
+                ))
+              ) : (
+                // TODO - Add a spinner
+                <h1>Loading....</h1>
+              )}
+            </>
+          </tbody>
+        </table>
+      </Container>
     </Container>
   );
 };
