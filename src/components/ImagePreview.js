09bd88dd7b0a7d301dda239136ca69fb4df6b0a3
Mike Ralph <michael_ralph@hotmail.com>
2023-01-28 18:21:35 +0000

    Add a glow to the images when the user hovers over them

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/ImagePreview.module.css";
 import plusIcon from "../assets/plus_icon.png";
 
 const ImagePreview = ({ src, height = 45, width = 45 }) => {
+  const [isHovering, setIsHovering] = useState(false);
+
   if (src === "") {
     src = plusIcon;
   }
 
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
     <span>
       <img
-        className={styles.ImagePreview}
+        className={
+          isHovering ? `${styles.ImagePreviewHover}` : `${styles.ImagePreview}`
+        }
         src={src}
         height={height}
         width={width}
         alt="File Preview"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
       ></img>
     </span>
   );
