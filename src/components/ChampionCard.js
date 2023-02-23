f5cff92eff32e559e7af3db4ad20159109caa348
Mike Ralph <michael_ralph@hotmail.com>
2023-02-23 19:05:24 +0000

    Apply link to the entire card not just the name

--- src/components/ChampionCard.js
+++ src/components/ChampionCard.js
@@ -9,18 +9,18 @@ import styles from "../styles/ChampionCard.module.css";
 const ChampionCard = (props) => {
   const { champ_image, name, id } = props;
   return (
-    <Card className={styles.ChampionCard}>
-      <Card.Img
-        className={styles.ChampionImage}
-        variant="top"
-        src={champ_image}
-      />
-      <Card.Body className={styles.ChampionCardBody}>
-        <Link to={`/champion/${id}`}>
+    <Link to={`/champion/${id}`}>
+      <Card className={styles.ChampionCard}>
+        <Card.Img
+          className={styles.ChampionImage}
+          variant="top"
+          src={champ_image}
+        />
+        <Card.Body className={styles.ChampionCardBody}>
           <Card.Title className={styles.ChampionName}>{name}</Card.Title>
-        </Link>
-      </Card.Body>
-    </Card>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
 
